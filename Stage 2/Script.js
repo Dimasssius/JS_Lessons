a0de0d12ad75132a46ccb5f3b225abcdb8dea8b1
Dimasssius <Tj-dude@mail.ru>
2019-06-11 10:14:23 +0300

    Fourth commit

---
+++ Stage 2/Script.js
@@ -0,0 +1,40 @@
+var     money = +prompt("Ваш бюджет на месяц?", ""),
+        time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+var appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpencess: null,
+    income: null,
+    savinvings: false
+};
+
+
+for (var i=0; i < 2; i++) {
+    var a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+    
+    if ((typeof (a)) === "string" && ((typeof (a)) != null) && ((typeof (b)) != null)
+        && a != "" && b != "" && a.length < 50) {
+        console.log ("done");
+        appData.expenses[a] = b;
+    }    
+    
+};
+
+appData.moneyPerDay = appData.budget/30;
+
+alert("Ваш бюджет на день составляет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Ошибка");
+}
+
+console.log(appData);
